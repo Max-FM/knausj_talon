ca2814df6cc89a23d77d30108ad84280b355c977
Max Foxley-Marrable <22856173+Max-FM@users.noreply.github.com>
2023-03-31 10:45:26 -0400

    Add functionality for powertoys run

---
+++ apps/powertoys/powertoys.py
@@ -0,0 +1,16 @@
+from talon import Context, Module, actions
+
+mod = Module()
+ctx = Context()
+ctx.matches = r"""
+os: windows
+"""
+
+@mod.action_class
+class Actions:
+    def powertoys_run(search_text: str):
+        """Runs PowerToys Run"""
+        actions.key("escape")
+        actions.key("alt-space")
+        print("search_text: " + search_text)
+        actions.insert(search_text)
---
+++ apps/powertoys/powertoys.talon
@@ -0,0 +1,1 @@
+power run [<user.text>]$: user.powertoys_run(user.text or '')
